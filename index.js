e50939ff75eabf38aae6421adafbcc85e328fa66
paul-bot882 <a94053477@gmail.com>
2026-04-01 23:10:33 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('Pong! 🏓');
+  }
+
+  if (message.content === '!money') {
+    message.reply('Start earning smart today 💰');
+  }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
